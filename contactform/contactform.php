12df3378814a501ad46c1976f936594630bf4dce
Deepa <deepa@Deepas-MacBook-Pro.local>
2019-05-05 22:12:20 +0900

    [Database Changes]

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,5 +1,4 @@
 <?php
-
     // $db = pg_connect("host=ec2-79-125-4-72.eu-west-1.compute.amazonaws.com port=5432 dbname=dcuoqso0dff69g user=idnvsgjudopxup password=ae6271b8e68779c603d64f5915ac764992785969b7e6f059e21645b713bd45a1");
     $db = parse_url(getenv("DATABASE_URL") ?: "postgres://idnvsgjudopxup:ae6271b8e68779c603d64f5915ac764992785969b7e6f059e21645b713bd45a1@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/dcuoqso0dff69g");
     $pdo = new PDO("pgsql:" . sprintf(
@@ -19,9 +18,9 @@
     VALUES (:name, :email, :subject, :comment)');
 
     $statement->execute([
-        'name' => '".$_POST["fname"]."',
-        'email' => '".$_POST["email"]."',
-        'subject' => '".$_POST["subject"]."',
-        'comment' => '".$_POST["message"]."'
+        'name' => $_POST["name"],
+        'email' => $_POST["email"],
+        'subject' => $_POST["subject"],
+        'comment' => $_POST["message"]
     ]);
 ?>
